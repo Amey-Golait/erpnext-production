6a12a3d40addaeba746c163ed43dcbfa0c39bdbc
AmeyGolait <152851721+Amey-Golait@users.noreply.github.com>
2025-07-24 10:44:13 +0530

    Delete docker directory

--- docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/frappe/frappe-bench
-
-# Build production assets
-bench build --production --force
-
-# Start development server (for Render)
-bench serve --port ${PORT:-8000}
